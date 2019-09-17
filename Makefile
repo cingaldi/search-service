a08e9572255d83f338ea5e23f5d16a4eb3f9e334
cingaldi <cingaldi@scmgroup.com>
2019-09-17 18:23:13 +0200

    initial commit

---
+++ Makefile
@@ -0,0 +1,3 @@
+init:
+	. ./env/bin/activate; \
+	pip install -r requirements.txt \
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='search-service', 
+    version='1.0', 
+    author="Carmine Ingaldi",
+    author_email="ingaldi.csb@gmail.com"
+    packages=find_packages()
+    )
