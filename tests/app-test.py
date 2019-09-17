1d65bf901eb1db93f4e20b6f9b3ef2ea5702e49c
cingaldi <cingaldi@scmgroup.com>
2019-09-17 18:46:22 +0200

    added health endpoint

--- tests/app-test.py
+++ tests/app-test.py
@@ -1,1 +1,14 @@
 from src import app
+
+import unittest
+
+
+class BasicTestCase(unittest.TestCase):
+
+    def test_index(self):
+        tester = app.app.test_client(self)
+        response = tester.get('/health')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
