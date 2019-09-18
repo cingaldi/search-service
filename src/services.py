a9ce1444c02b5e92ee32a8d8ba3165c3fa3f65f9
cingaldi <cingaldi@scmgroup.com>
2019-09-18 20:29:37 +0200

    added pgraphics service

--- src/services.py
+++ src/services.py
@@ -1,11 +1,44 @@
 from src import models
 
+class IService:
+    def getBase(self):
+        pass
+
+    def getFiltered(self , value):
+        pass
+
+class CoreService(IService):
+
+    def __init__ (self , dataSource , navigateKey , filterKey):
+        self.__treeModel = models.TreeModelFactory(dataSource).getInstance()
+        self.__navigateKey = navigateKey
+        self.__filterKey = filterKey
+
+    def getBase(self):
+        return self.__treeModel.navigateBy(self.__navigateKey).getLeaves()
+
+    def getFiltered(self , value):
+        return self.__treeModel.navigateBy(self.__navigateKey).filterBy(self.__filterKey , value).getLeaves()       
+
 class CategoriesService:
 
     def __init__ (self):
-        self.__treeModel = models.TreeModelFactory("data/categories").getInstance()
+        self.__s = CoreService("data/categories" , "children" , "name")
     def getBase(self):
-        return self.__treeModel.navigateBy("children").getLeaves()
+        return self.__s.getBase()
 
     def getFiltered(self , value):
-        return self.__treeModel.navigateBy("children").filterBy("name" , value).getLeaves()
+        return self.__s.getFiltered(value)
+
+class PsychographicsService:
+
+    def __init__ (self):
+        self.__s = CoreService("data/psychographics" , "values" , "label")
+    def getBase(self):
+        return self.__s.getBase()
+
+    def getFiltered(self , value):
+        print(value)
+        return self.__s.getFiltered(value)
+
+
--- tests/services-tests.py
+++ tests/services-tests.py
@@ -21,6 +21,25 @@ class CategoriesServiceTestCase(unittest.TestCase):
         assert result is not None
         assert result[0]["name"] == "Cars"
 
+class PsychographicsServiceTestCase(unittest.TestCase):
+
+    def test_getBase(self):
+
+        service = services.PsychographicsService ()
+
+        result = service.getBase()
+
+        assert result is not None
+        assert result[0]["value"] == "Ads web"
+
+    def test_getFiltered(self):
+        service = services.PsychographicsService ()
+
+        result = service.getFiltered("Music")
+
+        assert result is not None
+        assert result[0]["value"] == "Background beats"
+
 
 if __name__ == '__main__':
     unittest.main()
