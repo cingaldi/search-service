09ff518936628ff98e9b912f4b3000788de99a70
cingaldi <cingaldi@scmgroup.com>
2019-09-18 20:05:05 +0200

    implemented categories service

--- src/services.py
+++ src/services.py
@@ -5,7 +5,7 @@ class CategoriesService:
     def __init__ (self):
         self.__treeModel = models.TreeModelFactory("data/categories").getInstance()
     def getBase(self):
-        return self.__treeModel.getLeaves()
+        return self.__treeModel.navigateBy("children").getLeaves()
 
     def getFiltered(self , value):
-        return self.__treeModel.filterBy("name" , value).getLeaves()
+        return self.__treeModel.navigateBy("children").filterBy("name" , value).getLeaves()
